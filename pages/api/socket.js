a4c9097e4e57a0b58f50c1e43e78364d1330dd97
nowherefaster <max_choong@hotmail.com>
2025-08-21 22:48:10 +0100

    Update socket.js

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,42 +1,62 @@
-// This is the API route for our server
+// This is our serverless function that will handle WebSocket connections
 import { Server } from 'socket.io';
+import http from 'http';
 
-const SocketHandler = (req, res) => {
-  // If the server is already running, we don't need to create a new one
-  if (res.socket.server.io) {
-    console.log('Socket is already running');
-  } else {
-    console.log('Socket is initializing');
-    // Create a new Socket.IO server on top of our existing HTTP server
-    const io = new Server(res.socket.server);
+// We create a global variable to store the socket server instance
+// This is to prevent creating multiple servers on hot reloads in development
+let io;
 
-    // This is the main connection handler
-    io.on('connection', (socket) => {
-      console.log('A user connected:', socket.id);
+// The main handler for our serverless function
+export default function handler(req, res) {
+  // If the Socket.IO server is already created, we can skip the setup
+  if (io) {
+    res.end();
+    return;
+  }
 
-      // Listen for the 'join-room' event from the client
-      socket.on('join-room', (data) => {
-        const { roomid, user } = data;
-        // Join the specified room
-        socket.join(roomid);
-        console.log(`${user} joined room: ${roomid}`);
+  // Otherwise, we create a new HTTP server instance
+  const httpServer = new http.Server(req.socket.server);
+  
+  // Create a new Socket.IO server and attach it to the HTTP server
+  io = new Server(httpServer, {
+    path: '/api/socket' // We must specify the path here to match the client
+  });
 
-        // Emit a message to all clients in the same room, including the sender
-        io.to(roomid).emit('user-joined', { user });
+  // Handle the 'connection' event when a new client connects
+  io.on('connection', (socket) => {
+    console.log(`New client connected: ${socket.id}`);
 
-        // A broadcast message to the sender only
-        socket.emit('update-room', { message: `Welcome to room ${roomid}, ${user}` });
+    // Handle the 'join-room' event from the client
+    socket.on('join-room', ({ roomid, user }) => {
+      // Join the specified room
+      socket.join(roomid);
+      console.log(`${user} joined room ${roomid}`);
+
+      // Emit a message back to the user who just joined
+      socket.emit('update-room', {
+        message: `Welcome to room ${roomid}, ${user}`
       });
 
-      // Listen for a 'disconnect' event from the client
-      socket.on('disconnect', () => {
-        console.log('A user disconnected:', socket.id);
+      // Broadcast a message to all other users in the room
+      socket.broadcast.to(roomid).emit('user-joined', {
+        user: user
       });
     });
 
-    res.socket.server.io = io;
-  }
-  res.end();
-};
+    // Handle the 'disconnect' event when a client leaves
+    socket.on('disconnect', () => {
+      console.log(`Client disconnected: ${socket.id}`);
+    });
+  });
 
-export default SocketHandler;
+  // Attach the Socket.IO server to the HTTP server's 'upgrade' event
+  httpServer.listen(0, '0.0.0.0', () => {
+    // The following lines are necessary for Vercel to handle the connection
+    req.socket.server.on('upgrade', (req, socket, head) => {
+      io.engine.handleUpgrade(req, socket, head);
+    });
+  });
+
+  // End the response to allow the serverless function to complete
+  res.end();
+}
